583eec02bb26e4573648d4914686eee6cc7d2115
Mikhail Kirillov <mikkirillov@yandex.ru>
2019-03-27 11:28:57 +0300

    change deps

--- cmd/bolt/main_test.go
+++ cmd/bolt/main_test.go
@@ -12,8 +12,8 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/boltdb/bolt"
-	"github.com/boltdb/bolt/cmd/bolt"
+	"github.com/belfinor/bolt"
+	"github.com/belfinor/bolt/cmd/bolt"
 )
 
 // Ensure the "info" command can print information about a database.
